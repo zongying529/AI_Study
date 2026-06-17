491cbb7a098e9e7b9bfc2ddac287597130a09f42
Wow薇 <676894376@qq.com>
2026-06-17 18:45:25 +0800

    生成了一个新的文件 requirementes.txt，并修改了 test.py 文件。

---
+++ test.py
@@ -0,0 +1,13 @@
+import torch
+
+print("CUDA available:", torch.cuda.is_available())
+print("CUDA device count:", torch.cuda.device_count())
+
+
+if torch.cuda.is_available():
+    print("Current CUDA device:", torch.cuda.current_device())
+    print("CUDA device name:", torch.cuda.get_device_name(0))
+    print("CUDA device version:", torch.version.cuda)
+    
+else:
+    print("CUDA is not available")
